5d2d0b416807e583e248958807678f6b011219b0
何冠峰 <hevinci@hotmail.com>
2025-02-11 10:20:33 +0800

    fix #341

--- Assets/YooAsset/Samples~/Extension Sample/Runtime/ExtensionFileSystem/WechatFileSystem/Operation/WXFSDownloadFileOperation.cs
+++ Assets/YooAsset/Samples~/Extension Sample/Runtime/ExtensionFileSystem/WechatFileSystem/Operation/WXFSDownloadFileOperation.cs
@@ -52,6 +52,12 @@ internal class WXFSDownloadFileOperation : DefaultDownloadFileOperation
                 _steps = ESteps.Done;
                 Status = EOperationStatus.Succeed;
                 _fileSystem.TryRecordBundle(Bundle); //记录下载文件
+
+                //TODO 解决微信小游戏插件问题
+                // Issue : https://github.com/wechat-miniprogram/minigame-unity-webgl-transform/issues/108#
+                DownloadProgress = 1f;
+                DownloadedBytes = Bundle.FileSize;
+                Progress = 1f;
             }
             else
             {
