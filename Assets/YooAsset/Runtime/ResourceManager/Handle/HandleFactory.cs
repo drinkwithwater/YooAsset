772ba484eb8e9acc87659b76fd229128053c8475
何冠峰 <hevinci@hotmail.com>
2025-03-08 15:01:19 +0800

    Update HandleFactory.cs

--- Assets/YooAsset/Runtime/ResourceManager/Handle/HandleFactory.cs
+++ Assets/YooAsset/Runtime/ResourceManager/Handle/HandleFactory.cs
@@ -5,7 +5,7 @@ namespace YooAsset
 {
     internal static class HandleFactory
     {
-        private static readonly Dictionary<Type, Func<ProviderOperation, HandleBase>> _handleFactory = new()
+        private static readonly Dictionary<Type, Func<ProviderOperation, HandleBase>> _handleFactory = new Dictionary<Type, Func<ProviderOperation, HandleBase>>()
         {
             { typeof(AssetHandle), op => new AssetHandle(op) },
             { typeof(SceneHandle), op => new SceneHandle(op) },
